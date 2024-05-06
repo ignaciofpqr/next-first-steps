e567af0947ec862ab372b3cf8f663da85c853d5f
Ignacio Peralta <ignaciofpqr@gmail.com>
2024-05-06 17:31:25 -0300

    one change

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -13,21 +13,21 @@ const navItems = [
 
 export const Navbar = () => {
 
-  
+
 
   return (
     <nav className="flex bg-blue-800 bg-opacity-30 p-2 m-2 rounded">
 
       <Link href={'/'} className='flex items-center'>
         <HomeIcon className='mr-2' />
-        <span>Home</span>
+        <span>Home!</span>
       </Link>
 
       <div className="flex flex-1"></div>
 
       {
-        navItems.map( navItem => (
-          <ActiveLink key={navItem.path} { ...navItem }  />
+        navItems.map(navItem => (
+          <ActiveLink key={navItem.path} {...navItem} />
         ))
       }
 
